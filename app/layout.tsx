48a41236075de8ff1fa1ba683db0b7a85d37cc7b
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2025-09-07 15:38:52 +0530

    Update: Middleware Logic to access the protected route only while sign in

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { auth } from "@/auth";
-import {SessionProvider} from "next-auth/react";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,16 +24,14 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth()
+  const session = await auth();
   return (
-    <SessionProvider session={session}>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers session={session}>
+          {children}
+        </Providers>
       </body>
     </html>
-    </SessionProvider>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,46 @@
 import NextAuth from "next-auth";
 
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  publicRoutes,
+  authRoutes,
+} from "@/routes";
 import authConfig from "./auth.config";
-import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
 
-const {auth} = NextAuth(authConfig);
 
-export default auth((req)=>{
-    const {nextUrl} = req;
-    const isLoggedIn = !!req.auth;
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-    
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+const { auth } = NextAuth(authConfig);
 
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+// @ts-ignore
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-    if(isApiAuthRoute){
-        return null;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    if(isAuthRoute){
-        if(isLoggedIn){
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-        return null;
-    }
-})
+    return null;
+  }
+
+  if(!isLoggedIn && !isPublicRoute){
+    return Response.redirect(new URL("/auth/sign-in" , nextUrl))
+  }
+
+  return null
+});
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  // copied from clerk
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
