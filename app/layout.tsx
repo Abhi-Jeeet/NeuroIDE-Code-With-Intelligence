d339653ea2b65266257421a256f9a1d3c9a95837
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2025-09-07 18:01:24 +0530

    Feat: Add theme support and header components
    Introduces ThemeProvider and ThemeToggle for dark/light mode switching. Adds header and footer components, updates layout structure, and moves home page files to a new directory for improved organization.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { auth } from "@/auth";
 import { Providers } from "./providers";
+import { ThemeProvider } from "@/components/ui/providers/theme-provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +28,18 @@ export default async function RootLayout({
 }>) {
   const session = await auth();
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <Providers session={session}>
           {children}
         </Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
