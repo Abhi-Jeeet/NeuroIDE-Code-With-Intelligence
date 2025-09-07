c216be398d8480ca15217b6f5de12f56c758beec
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2025-09-07 16:45:11 +0530

    Feat: Authentication Implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import UserButton from "@/features/auth/components/user-button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-rose-500 text-3xl">Hello</h1>
-      <Button>Click me</Button>
+      <UserButton/>
     </div>
   );
 }
---
+++ features/auth/components/logout-button.tsx
@@ -0,0 +1,23 @@
+import React, { Children } from "react";
+import {LogoutButtonProps} from "../type";
+
+import { useRouter } from "next/navigation";
+import {signOut} from "next-auth/react";
+
+const LogOutButton=({children}: LogoutButtonProps)=>{
+    const router = useRouter();
+    
+    const onLogout= async()=>{
+        await signOut();
+        router.refresh()
+    }
+
+    return(
+    <span className="cursor-pointer" onClick={onLogout}>
+        {children}
+    </span>
+)
+}
+
+export default LogOutButton;
+
