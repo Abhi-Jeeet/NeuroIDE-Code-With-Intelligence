9fc7d91be4f01c3aef7c463269ab4268472b14d8
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2025-09-07 14:33:12 +0530

    feat: Add authentication flow with NextAuth and UI
    Introduces authentication pages and layout, sign-in form component, and server actions for user/account retrieval. Updates NextAuth configuration and session typing, integrates SessionProvider in app layout, and refines Prisma schema. Enables Google and GitHub sign-in options.

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import SignInFormClient from '@/features/auth/components/signin-form-client'
+
+const SignInPage = () => {
+  return (
+    <>
+    {/* <Image/> */}
+    <h1 className='text-white'>logo</h1>
+    <SignInFormClient/>
+
+    
+    </>
+  )
+}
+
+export default SignInPage
--- auth.config.ts
+++ auth.config.ts
@@ -1,16 +1,16 @@
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "next-auth"
+
 
 export default{
     providers:[
         GitHub({
-            clientId:process.env.AUTH_GITHUB_ID,
-            clientSecret:process.env.AUTH_GITHUB_SECRET
+            clientId:process.env.GITHUB_ID,
+            clientSecret:process.env.GITHUB_SECRET
         }),
         Google({
-            clientId:process.env.AUTH_GOOGLE_ID,
-            clientSecret:process.env.AUTH_GOOGLE_SECRET,
+            clientId:process.env.GOOGLE_ID,
+            clientSecret:process.env.GOOGLE_SECRET,
         })
     ]
-} satisfies NextAuthConfig
+} 
