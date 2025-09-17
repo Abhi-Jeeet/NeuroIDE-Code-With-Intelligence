955788976492bda31310aad2953440dcd2e6271b
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2025-09-17 12:18:05 +0530

    Update: Adding title and description

--- features/playground/actions/index.ts
+++ features/playground/actions/index.ts
@@ -9,6 +9,8 @@ export const getPlaygroundById = async(id:string)=>{
         const playground = await db.playground.findUnique({
             where:{id},
             select:{
+                title:true,
+                description:true,
                 templateFiles:{
                     select:{
                         content:true
