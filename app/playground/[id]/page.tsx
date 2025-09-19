93cec0b8023213c4c21c15ff4543d6a29e815e8a
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2025-09-19 19:56:10 +0530

    Feat: Add file explorer state management

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -2,13 +2,102 @@
 import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
 import TemplateFileTree from '@/features/playground/components/template-file-tree';
+import { useFileExplorer } from '@/features/playground/hooks/useFileExplorer';
 import { usePlayground } from '@/features/playground/hooks/usePlayground';
+import { TemplateFile, TemplateFolder } from '@/features/playground/types';
 import { useParams } from 'next/navigation';
-import React from 'react'
+import { writeFileSync } from 'node:fs';
+import React, { useCallback } from 'react'
 
 const page = () => {
     const {id} = useParams<{id:string}>();
     const {playgroundData, templateData, isLoading, error, saveTemplateData} = usePlayground(id)
+    const {
+      activeFileId,
+      closeAllFiles,
+      openFile,
+      closeFile,
+      editorContent,
+      updateFileContent,
+      handleAddFile,
+      handleAddFolder,
+      handleDeleteFile,
+      handleDeleteFolder,
+      handleRenameFile,
+      handleRenameFolder,
+      openFiles,
+      setTemplateData,
+      setActiveFileId,
+      setPlaygroundId,
+      setOpenFiles
+
+    } = useFileExplorer()
+
+    const wrappedHandleAddFile = useCallback(
+    (newFile: TemplateFile, parentPath: string) => {
+      return handleAddFile(
+        newFile,
+        parentPath,
+        writeFileSync!,
+        instance,
+        saveTemplateData
+      );
+    },
+    [handleAddFile, writeFileSync, instance, saveTemplateData]
+  );
+
+  const wrappedHandleAddFolder = useCallback(
+    (newFolder: TemplateFolder, parentPath: string) => {
+      return handleAddFolder(newFolder, parentPath, instance, saveTemplateData);
+    },
+    [handleAddFolder, instance, saveTemplateData]
+  );
+
+  const wrappedHandleDeleteFile = useCallback(
+    (file: TemplateFile, parentPath: string) => {
+      return handleDeleteFile(file, parentPath, saveTemplateData);
+    },
+    [handleDeleteFile, saveTemplateData]
+  );
+
+  const wrappedHandleDeleteFolder = useCallback(
+    (folder: TemplateFolder, parentPath: string) => {
+      return handleDeleteFolder(folder, parentPath, saveTemplateData);
+    },
+    [handleDeleteFolder, saveTemplateData]
+  );
+
+  const wrappedHandleRenameFile = useCallback(
+    (
+      file: TemplateFile,
+      newFilename: string,
+      newExtension: string,
+      parentPath: string
+    ) => {
+      return handleRenameFile(
+        file,
+        newFilename,
+        newExtension,
+        parentPath,
+        saveTemplateData
+      );
+    },
+    [handleRenameFile, saveTemplateData]
+  );
+
+  const wrappedHandleRenameFolder = useCallback(
+    (folder: TemplateFolder, newFolderName: string, parentPath: string) => {
+      return handleRenameFolder(
+        folder,
+        newFolderName,
+        parentPath,
+        saveTemplateData
+      );
+    },
+    [handleRenameFolder, saveTemplateData]
+  );
+
+
   return (
     <div>
        <>
