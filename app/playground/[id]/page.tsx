5e80fff8a1d7b0e603e788316c6c8f588c45efb8
Abhi-Jeeet <157899302+Abhi-Jeeet@users.noreply.github.com>
2025-09-23 20:37:12 +0530

    Feeat: Add a button to save, save all, toggle ai, and setting button

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,37 +1,49 @@
 "use client";
-import { Separator } from '@/components/ui/separator';
-import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
-import TemplateFileTree from '@/features/playground/components/template-file-tree';
-import { useFileExplorer } from '@/features/playground/hooks/useFileExplorer';
-import { usePlayground } from '@/features/playground/hooks/usePlayground';
-import { TemplateFile, TemplateFolder } from '@/features/playground/types';
-import { useParams } from 'next/navigation';
-import { writeFileSync } from 'node:fs';
-import React, { useCallback } from 'react'
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import TemplateFileTree from "@/features/playground/components/template-file-tree";
+import { useFileExplorer } from "@/features/playground/hooks/useFileExplorer";
+import { usePlayground } from "@/features/playground/hooks/usePlayground";
+import { TemplateFile, TemplateFolder } from "@/features/playground/types";
+import { TooltipContent } from "@radix-ui/react-tooltip";
+import { Bot, Save, Settings } from "lucide-react";
+import { useParams } from "next/navigation";
+import { writeFileSync } from "node:fs";
+import React, { useCallback } from "react";
 
 const page = () => {
-    const {id} = useParams<{id:string}>();
-    const {playgroundData, templateData, isLoading, error, saveTemplateData} = usePlayground(id)
-    const {
-      activeFileId,
-      closeAllFiles,
-      openFile,
-      closeFile,
-      editorContent,
-      updateFileContent,
-      handleAddFile,
-      handleAddFolder,
-      handleDeleteFile,
-      handleDeleteFolder,
-      handleRenameFile,
-      handleRenameFolder,
-      openFiles,
-      setTemplateData,
-      setActiveFileId,
-      setPlaygroundId,
-      setOpenFiles
+  const { id } = useParams<{ id: string }>();
+  const { playgroundData, templateData, isLoading, error, saveTemplateData } =
+    usePlayground(id);
+  const {
+    activeFileId,
+    closeAllFiles,
+    openFile,
+    closeFile,
+    editorContent,
+    updateFileContent,
+    handleAddFile,
+    handleAddFolder,
+    handleDeleteFile,
+    handleDeleteFolder,
+    handleRenameFile,
+    handleRenameFolder,
+    openFiles,
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+  } = useFileExplorer();
 
-    } = useFileExplorer()
+  const activeFile = openFiles.find((file) => file.id === activeFileId);
+  const hasUnsavedChanges = openFiles.some((file) => file.hasUnsavedChanges);
 
   //   const wrappedHandleAddFile = useCallback(
   //   (newFile: TemplateFile, parentPath: string) => {
@@ -97,32 +109,88 @@ const page = () => {
   //   [handleRenameFolder, saveTemplateData]
   // );
 
-
   return (
-    <div>
-       <>
-       {/* TODO: TEMPLATES TREE */}
-       <TemplateFileTree
-       data={templateData!}/>
-       <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
-            <SidebarTrigger className='-ml-1'/>
-            <Separator orientation='vertical' className='mr-2 h-4'/>
-            <div className='flex flex-1 items-center gap-2'>
-                <div className='flex flex-col flex-1'>
+    <TooltipProvider>
+      <>
+        {/* TODO: TEMPLATES TREE */}
+        <TemplateFileTree data={templateData!} />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <div className="flex flex-1 items-center gap-2">
+              <div className="flex flex-col flex-1">
+                <h1 className="text-sm font-medium">
                   {playgroundData?.title || "Code Playground"}
-                    
+                </h1>
+                <p className="text-xs text-muted-foreground">
+                  {openFiles.length} File(s) open
+                  {hasUnsavedChanges && " . Unsaved changes"}
+                </p>
+              </div>
+              <div className="flex items-center-gap-1">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      size={"sm"}
+                      variant={"outline"}
+                      onClick={() => {}}
+                      disabled={!activeFile || !activeFile.hasUnsavedChanges}
+                    >
+                      <Save className="size-4" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    Save (Ctrl + S)
+                  </TooltipContent>
+                </Tooltip>
 
-                </div>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      size={"sm"}
+                      variant={"outline"}
+                      onClick={() => {}}
+                      disabled={!hasUnsavedChanges}
+                    >
+                      <Save className="h-4 w-4" /> All
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    Save All (Ctrl + Shift + S)
+                  </TooltipContent>
+                </Tooltip>
 
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      size={"sm"}
+                      variant={"outline"}
+                      onClick={() => {}}
+                      disabled={!hasUnsavedChanges}
+                    >
+                      <Bot className="h-4 w-4" /> ToggleAI
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    ToggleAI
+                  </TooltipContent>
+                </Tooltip>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button size={"sm"} variant={"outline"}>
+                      <Settings className="size-4"/>
+                    </Button>
+
+                  </DropdownMenuTrigger>
+                </DropdownMenu>
+              </div>
             </div>
+          </header>
+        </SidebarInset>
+      </>
+    </TooltipProvider>
+  );
+};
 
-        </header>
-       </SidebarInset>
-       </>
-    </div>
-    
-  )
-}
-
-export default page
+export default page;
